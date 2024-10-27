739fd14570772fd825db0dbef518b578e686ec27
Cristian <criscamcr7@gmail.com>
2024-10-26 21:07:10 -0500

    Octava prueba vercel.json

--- Asambly/Asambly/settings.py
+++ Asambly/Asambly/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'Asambly',
     'apiAsambly.apps.ApiasamblyConfig',
     'rest_framework'
@@ -54,21 +53,6 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'Asambly.urls'
 
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
 
 WSGI_APPLICATION = 'Asambly.wsgi.application'
 
@@ -120,9 +104,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-
-STATIC_URL = 'static/'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
